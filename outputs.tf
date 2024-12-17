09fe8603b20ec3c5660f3e4175a2a1009c58d816
i505083 <tibor.kiss01@sap.com>
2024-12-17 16:51:07 +0100

    Switch to using tfe_outputs

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@
 ##################################################################################
 
 output "vpc_id" {
-  value = module.main.vpc_id
+  value       = module.main.vpc_id
   description = "VPC ID"
 }
 
 output "public_subnets" {
-    value = module.main.public_subnets
-    description = "List of public subnets"
-  
+  value       = module.main.public_subnets
+  description = "List of public subnets"
+
 }
